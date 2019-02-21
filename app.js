5c73dd2aac8e76d4b6f12bec1d87b561b8ea9464
Ma Vang <ma.vang@umontana.edu>
2019-02-20 20:07:06 -0700

    updated todo list. Implemented in css formatting. Implemented in functionaility to display inspi rational quotes.

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 const express = require("express"); 
 const bodyParser = require("body-parser"); 
+const quotes = require("inspirational-quotes");
+const timer = require("node-get-time");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.set('view engine', 'ejs'); 
-var items = ["Pack Food", "Work", "Learn code", "Sleep"]; 
+
+let items = ["Eat", "Code", "Sleep"]; 
 
 app.get("/", function(req, res){
-    var today = new Date();
-    var dayDescription = "Here's your todo list for today."; 
-    var options = {
+    
+    let today = new Date();
+    let dayDescription ='"'+ quotes.getRandomQuote() + '"' ; 
+    let options = {
         weekday: 'long', 
         year: 'numeric', 
         month: 'long', 
         day:'numeric'};
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
    
     res.render("list", 
     {
         DAY: day, 
         DayDescription: dayDescription,
         newListItems: items
-    })
+    });
     
 });
 
 app.post("/", function(request, response){
-    var item = request.body.newItem;
+    let item = request.body.newItem;
     items.push(item);
     response.redirect("/");
 });
@@ -34,3 +39,5 @@ app.post("/", function(request, response){
 app.listen(3000, function(){
     console.log("Server started on port 3000"); 
 });
+
+
